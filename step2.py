d305b5ad0e60d9ed14a8fe069f3fed59296b4176
CYJ001 <chanel_johnson@brown.edu>
2016-10-29 16:58:43 -0400

    Update step2.py

--- step2.py
+++ step2.py
@@ -1,8 +1,6 @@
 import requests
 a = {"token" : "61d1f5b7ef20dacafcf93addf4c1fc0c"}
 reqOne = requests.post('http://challenge.code2040.org/api/reverse', json=a)
-reqTwo = requests.get('http://challenge.code2040.org/api/reverse')
-#print(reqTwo.text)
-newString = reqTwo.text[::-1]
+newString = reqOne.text[::-1]
 b = {"token" : "61d1f5b7ef20dacafcf93addf4c1fc0c", "string" : newString}
-reqThree = requests.post('http://challenge.code2040.org/api/reverse/validate', json=b)
+reqTwo = requests.post('http://challenge.code2040.org/api/reverse/validate', json=b)
